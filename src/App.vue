dd08061e976d27d275cf2a9056a7238b98039845
Reicela <reicelapetersone@gmail.com>
2022-10-27 21:03:28 +0300

    clock created

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <DigitalClock />
+</template>
+
+<script>
+import DigitalClock from "./components/DigitalClock.vue";
+export default {
+  name: "App",
+  components: {
+    DigitalClock: DigitalClock,
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  padding-top: 5px;
+  background-image: url("https://images.pexels.com/photos/4367584/pexels-photo-4367584.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+}
+
+.time-date {
+  font-family: Lucida Console, Arial;
+  text-align: center;
+  margin-top: 200px;
+  color:rgb(199, 236, 236);
+  font-weight: bold;
+  font-size: 60px;
+}
+</style>
---
+++ src/components/DigitalClock.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="time-date">
+      <p>{{ currentTime.toLocaleTimeString() }}</p>
+      <p>{{ currentTime.toLocaleDateString() }}</p>
+    </div>
+</template>
+<script>
+import { useCurrentTime } from "../composables/useCurrentTime";
+export default {
+  name: "CurrentTimeExample",
+  setup() {
+    const { currentTime } = useCurrentTime();
+    console.log(currentTime.value);
+    return { currentTime };
+  },
+};
+</script>
